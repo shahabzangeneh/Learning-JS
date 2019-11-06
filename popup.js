b6b72e5244766bd5cce114711e838c9e161ff4d1
Mete0r <zanganehshahab@gmail.com>
2019-11-06 17:58:57 +0330

    end of popup project

--- popup.js
+++ popup.js
@@ -1,2 +1,18 @@
+const popupWrapper = document.querySelector('.popup-wrapper');
+const button = document.querySelector('button');
+const closeButton = document.querySelector('.popup-close');
 
+button.addEventListener('click' , () => {
+    popupWrapper.style.display = 'block';
+    console.log('event in button event listener');
+});
+
+closeButton.addEventListener('click' , ev =>{
+    popupWrapper.style.display ='none';
+    console.log('event in closebutton event listener');
+});
+popupWrapper.addEventListener('click' , () =>{
+    popupWrapper.style.display = 'none';
+    console.log('event in popup-wrapper event listener');
+});
 
